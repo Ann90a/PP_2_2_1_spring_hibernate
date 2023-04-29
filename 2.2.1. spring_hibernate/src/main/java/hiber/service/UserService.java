62f60e57a71699e3779bf043284db59f141ce528
comp <abrosimova222@yandex.ru>
2023-04-30 00:04:14 +0300

    2.2.1 выполнено

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -1,10 +1,15 @@
 package hiber.service;
 
 import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface UserService {
     void add(User user);
+
     List<User> listUsers();
+
+    @Transactional
+    List<User> getUserCar(String madel, int series);
 }
